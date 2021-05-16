677866f9f019ce38e3cad18d7c5fea5a8396acee
Yaroslav Yakymiv <yakymiv.0706@gmail.com>
2021-05-16 16:29:25 +0300

    little changes, add readme

---
+++ client/build/precache-manifest.d33749425a935b4acb44a9e6e966f866.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42c827dff777f8413b3e666fe7fabdde",
+    "url": "/index.html"
+  },
+  {
+    "revision": "711df7bfd7b582378059",
+    "url": "/static/js/2.e81f22ca.chunk.js"
+  },
+  {
+    "revision": "259c19f05e1b97f46113",
+    "url": "/static/js/main.8484b95c.chunk.js"
+  },
+  {
+    "revision": "dd27c32fde923620d4a7",
+    "url": "/static/js/runtime-main.6034ec44.js"
+  }
+]);
---
+++ readme.md
@@ -0,0 +1,11 @@
+For this project was used next technologies/libraries:
+    Client side:
+        * React, 
+        * Redux,
+        * Styled-Components
+
+    Server side: 
+        * Node js
+        * Express
+        * Mongodb
+        * Mongoosejs
